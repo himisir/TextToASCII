a6df9f814dbf61877e280935a986c7db77181d59
Abdullah Al Asif <31031926+himisir@users.noreply.github.com>
2017-11-28 12:37:01 +0600

    Add files via upload
    This code is written in C Programming Language. Feel free to feedback!

---
+++ TTA.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[1000000], n, i;
+    scanf("%[^\t]s",&a);
+    n=strlen(a);
+    for(i=0; i<n; i++)
+    {
+        if(a[i]==32) printf(" ");
+        else if(a[i]==10) printf("\n");
+        else if(a[i]==39) printf(",");
+        else if(a[i]==33) printf("!");
+        else if(a[i]==63) printf("?");
+        else
+        {
+            if(a[i]<100&&a[i]!=a[0]) printf("0%d", a[i]);
+            else printf("%d", a[i]);
+        }
+
+    }
+}
